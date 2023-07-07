5a7bf486be2939942742a3e42a471e74dc31f299
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-07-07 12:45:24 +0430

    Complete all exercises

--- mongo-exercise.js
+++ mongo-exercise.js
@@ -36,14 +36,34 @@ async function createCourse() {
 
 // createCourse()
 
-async function getCourses() {
-  const backendPublishedCourses = await Course.find({
+function getBackendCourses() {
+  return Course.find({
     $and: [{ tags: "backend" }, { isPublished: true }],
   })
     .sort({ name: 1 })
     .select({ name: 1, author: 1, _id: 0 });
-
-  console.log(backendPublishedCourses);
 }
 
-getCourses()
+function getFrontendBackendCourses() {
+  return Course.find({
+    isPublished: true,
+    // $or: [{ tags: "frontend" }, { tags: "backend" }],// this is one way to get courses thar are frontend and backend
+    tags:{$in:["frontend", "backend"]}
+  })
+    .sort( "-price" ) // we can use -1 or -price (-propertyName) to represent descending order same thing goes for ascending order
+    // .select({ name: 1, author: 1 }); // one way of selecting properties
+    .select([ "name", "author", "price", "-_id"]); // other way of selecting properties
+}
+
+async function getPublishedCourses(){
+
+  return Course.find({isPublished:true}).or([{price:{$gte: 15}}, {name:/.*by.*/i}])
+}
+async function run() {
+  // const result = await getBackendCourses()
+  // const result = await getFrontendBackendCourses();
+  const result = await getPublishedCourses();
+  console.log(result);
+}
+
+run();
